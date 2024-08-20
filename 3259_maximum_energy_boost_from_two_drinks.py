a96ea7285d4f1d81f5b12239a6d3cc23c111f6f2
Mario Cervera <mariocervera@gmail.com>
2024-08-21 00:28:00 +0200

    Minor refactor in solution to problem 3259

--- 3259_maximum_energy_boost_from_two_drinks.py
+++ 3259_maximum_energy_boost_from_two_drinks.py
@@ -1,11 +1,10 @@
 
 def maxEnergyBoost(energyDrinkA, energyDrinkB):
-    n = len(energyDrinkA)
     prev_prev_a, prev_prev_b = energyDrinkA[0], energyDrinkB[0]
     prev_a, prev_b = prev_prev_a + energyDrinkA[1], prev_prev_b + energyDrinkB[1]
-    for i in range(2, n):
-        current_a = max(prev_a + energyDrinkA[i], prev_prev_b + energyDrinkA[i])
-        current_b = max(prev_b + energyDrinkB[i], prev_prev_a + energyDrinkB[i])
+    for i in range(2, len(energyDrinkA)):
+        current_a = max(prev_a, prev_prev_b) + energyDrinkA[i]
+        current_b = max(prev_b, prev_prev_a) + energyDrinkB[i]
         prev_prev_a, prev_prev_b = prev_a, prev_b
         prev_a, prev_b = current_a, current_b
     return max(current_a, current_b)
