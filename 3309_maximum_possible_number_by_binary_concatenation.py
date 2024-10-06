0304e02cfee20b09b1972b202c1946e870ba26d9
Mario Cervera <mariocervera@gmail.com>
2024-10-06 23:41:47 +0200

    Add solution to problem 3309

---
+++ 3309_maximum_possible_number_by_binary_concatenation.py
@@ -0,0 +1,19 @@
+import functools
+
+
+def compare_integers(a, b):
+    if a == b:
+        return 0
+    return -1 if a + b > b + a else 1
+
+
+def maxGoodNumber(nums):
+    nums = [bin(num)[2:] for num in nums]
+    nums.sort(key=functools.cmp_to_key(compare_integers))
+    return int("".join(nums), 2)
+
+
+print(maxGoodNumber([1, 2, 3]))    # 30
+print(maxGoodNumber([1, 11, 5]))   # 221
+print(maxGoodNumber([2, 8, 16]))   # 1296
+print(maxGoodNumber([2, 67, 25]))  # 13123
