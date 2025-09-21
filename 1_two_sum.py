020bb51bb17e6421581696a1e177112472c9fdfa
Mario Cervera <mariocervera@gmail.com>
2025-09-22 01:14:52 +0200

    reformat file

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -12,4 +12,4 @@ def twoSum(nums, target):
 print(twoSum(nums=[2, 7, 11, 15], target=9))  # [0,1]
 print(twoSum(nums=[3, 2, 4], target=6))  # [1,2]
 print(twoSum(nums=[3, 3], target=6))  # [0,1]
-print(twoSum(nums =[2,5,5,11], target=10))  # [1,2]
+print(twoSum(nums=[2, 5, 5, 11], target=10))  # [1,2]
